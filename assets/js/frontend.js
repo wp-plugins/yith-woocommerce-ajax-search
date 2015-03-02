8b481930799a88f5d19730f4508ac106a153df98
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 10:09:17 +0000

    version 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/yith-woocommerce-ajax-search/tags/1.2.3@1103148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -5,28 +5,32 @@
  * @package YITH WooCommerce Ajax Search
  * @version 1.1.1
  */
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     "use strict";
 
-    var el = $('#yith-s'),
+    var el = $('.yith-s'),
         def_loader = ( typeof woocommerce_params != 'undefined' && typeof woocommerce_params.ajax_loader_url != 'undefined' ) ? woocommerce_params.ajax_loader_url : yith_wcas_params.loading,
         loader_icon = el.data('loader-icon') == '' ? def_loader : el.data('loader-icon'),
         min_chars = el.data('min-chars');
 
-    el.autocomplete({
-        minChars: min_chars,
-        appendTo: '.yith-ajaxsearchform-container',
-        serviceUrl: woocommerce_params.ajax_url + '?action=yith_ajax_search_products',
-        onSearchStart: function(){
-            $(this).css('background', 'url(' + loader_icon + ') no-repeat right center');
-        },
-        onSearchComplete: function(){
-            $(this).css('background', 'transparent');
-        },
-        onSelect: function (suggestion) {
-            if( suggestion.id != -1 ) {
-                window.location.href = suggestion.url;
+    el.each(function () {
+        var $t = $(this),
+            append_to = ( typeof  $t.data('append-to') == 'undefined') ? $t.closest('.yith-ajaxsearchform-container') : $t.data('append-to');
+
+        el.autocomplete({
+            minChars        : min_chars,
+            appendTo        : append_to,
+            serviceUrl      : woocommerce_params.ajax_url + '?action=yith_ajax_search_products',
+            onSearchStart   : function () {
+                $(this).css('background', 'url(' + loader_icon + ') no-repeat right center');
+            },
+            onSelect        : function (suggestion) {
+                if (suggestion.id != -1) {
+                    window.location.href = suggestion.url;
+                }
             }
-        }
+        });
     });
 });
+
+
